c7fbe5d41b2cfe0fbc726e5466ea5cafd67c6b0c
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-03-27 17:36:26 +0300

    working with while

--- day_6/3_while.py
+++ day_6/3_while.py
@@ -1,8 +1,9 @@
 from random import randint
 
+computer_number = randint(1, 10)
+print(computer_number)
 user_number = int(input("Enter Number from 1 to 10"))
 
-computer_number = randint(1, 10)
 
 tries = 1
 while user_number != computer_number:
@@ -14,3 +15,36 @@ if tries == 1:
 else:
     text = "tries"
 print("You Win After {} {}".format(tries, text))
+
+option = input("Enter Option l(list) / d(delete) / u(update)")
+#
+# options = ["l", "d", "u"]
+# while option not in options:
+#     option = input(
+#         f"Invalid option <{option}>, Type 'l' for (list) / 'd' for (delete) / 'u' for (update)"
+#     )
+#
+# if option == "l":
+#     print("Listing ....")
+#
+# elif option == "d":
+#     print("Deleting .....")
+#
+# elif option == "u":
+#     print("Updating .....")
+
+while 1:
+    option = input(
+        f"Invalid option <{option}>, Type 'l' for (list) / 'd' for (delete) / 'u' for (update)"
+    )
+    if option == "l":
+        print("Listing ....")
+
+    elif option == "d":
+        print("Deleting .....")
+
+    elif option == "u":
+        print("Updating .....")
+    else:
+        continue
+    break
