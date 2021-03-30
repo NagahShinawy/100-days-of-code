397136a8fda32ffb0c8755d40b50eb94a63daf3d
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-03-30 05:44:36 +0300

    flowchart

--- day_7/1_hangman.py
+++ day_7/1_hangman.py
@@ -1,6 +1,13 @@
+# from replit import clear , clear the screen like clear in linux or cls in windows (python 3.8)
+import os
 from random import choice
 from word_list import words
 from stages_list import stages
+
+
+def clear():
+    os.system("cls")
+
 
 word = choice(words)
 word_len = len(word)
@@ -11,6 +18,9 @@ print(display)
 
 def user_guess():
     guess = input("Enter Letter")
+    clear()  # clear screen
+    if guess in display:
+        print(f"You have already guessed this char '{guess}'")
     for index in range(word_len):
         letter = word[index]
         if letter == guess:
@@ -30,7 +40,7 @@ while not end_of_game:
         print("You Win", "".join(display))
         end_of_game = True
     if g not in word:
-        print(f'"{g}" is not at word. You lose a life')
+        print(f'"{g}" is not in word. You lose a life')
         lives -= 1
         print(stages[stage_index])
         stage_index += 1
